dca9b09ab5b199cf13b40a69c4e7002f0d5e26f3
JessicaAquino <jessica.aquino.torrez@gmail.com>
2020-07-02 15:44:54 -0400

    Actions: Saludar Alicia con las horas correctas

--- actions.py
+++ actions.py
@@ -34,9 +34,9 @@ class saludar(Action):
 
         if(hora >= 5) and (hora <= 11):
             texto = "Buen día 🌤 \nEstaba tomando un café ☕ \nMi nombre es ALICIA 🙋🏽 y resolveré tus dudas sobre el COVID-19! \n¿Cual es tu duda? 🤔"
-        elif (hora >= 12) and (hora <= 15):
+        elif (hora >= 12) and (hora <= 17):
             texto = "Buenas tardes 🌇 \nEstaba leyendo 📖 \nMi nombre es ALICIA 🙋🏽 y resolveré tus dudas sobre el COVID-19! \n¿Cual es tu duda? 🤔"
-        elif ((hora >= 16) and (hora <= 23)) or ((hora >= 0) and (hora <= 4)):
+        elif ((hora >= 18) and (hora <= 23)) or ((hora >= 0) and (hora <= 4)):
             texto = "Buenas noches 🌙 \nEstoy despierta 🦉 \nMi nombre es ALICIA 🙋🏽 y resolveré tus dudas sobre el COVID-19! \n¿Cual es tu duda? 🤔"
         else:
             texto = "Hola! Un gusto conocerte 👋🏽 \nMi nombre es ALICIA 🙋🏽 y resolveré tus dudas sobre el COVID-19! \n¿Cual es tu duda? 🤔"
