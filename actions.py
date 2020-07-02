5c1da2e94fe2885bcf445d52a6a9f82952bf5a9e
JessicaAquino <jessica.aquino.torrez@gmail.com>
2020-07-02 17:12:03 -0400

    Actions: Casos COVID-19 por departamento

--- actions.py
+++ actions.py
@@ -48,3 +48,53 @@ class menu_1(Action):
             })]
 
         return []
+
+class mostrarCasos(Action):
+
+    def name(self) -> Text:
+        return "actions_mostrar_casos"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        #Función que retorna los casos de COVID-19 según el departamento
+
+        if tracker.get_slot("departamento") != None:
+            url = 'https://raw.githubusercontent.com/mauforonda/covid19-bolivia/master/total.csv'
+            webpage = urlopen(url)
+            v_dp = tracker.get_slot("departamento").lower()
+            df = pd.read_csv(webpage)
+            ddf = pd.DataFrame(df)
+            nombreDepartamento = ""
+
+            if (v_dp == "la paz"):
+                nombreDepartamento = "La Paz"
+            elif (v_dp == "santa cruz"):
+                nombreDepartamento = "Santa Cruz"
+            elif (v_dp == "cochabamba"):
+                nombreDepartamento = "Cochabamba"
+            elif (v_dp == "oruro"):
+                nombreDepartamento = "Oruro"
+            elif (v_dp == "potosi"):
+                nombreDepartamento = "Potosi"
+            elif (v_dp == "tarija"):
+                nombreDepartamento = "Tarija"
+            elif (v_dp == "chuquisaca"):
+                nombreDepartamento = "Chuquisaca"
+            elif (v_dp == "beni"):
+                nombreDepartamento = "Beni"
+            elif (v_dp == "pando"):
+                nombreDepartamento = "Pando"
+            else:
+                dispatcher.utter_message(text="Lo siento no entendí el departamento, intenta escribirlo bien 🙌")
+                return[SlotSet("departamento", None)]
+
+
+            casosConfirmados = ddf[ddf['Province/State']==nombreDepartamento]['Confirmed'].item()
+            cantFallecidos = ddf[ddf['Province/State']==nombreDepartamento]['Deaths'].item()
+            cantRecuperados = ddf[ddf['Province/State']==nombreDepartamento]['Recovered'].item()
+
+            dispatcher.utter_message(text="En "+nombreDepartamento+" hay: \n*"+ str(casosConfirmados) +"* confirmados* ☑️ \n*"+ str(cantFallecidos)+"* decesos 📉 \n*"+str(cantRecuperados)+"* recuperados 💊")
+
+        return[SlotSet("departamento", None)]
