53a50c26856b2e12554451a5a789a8b2f95ada77
JessicaAquino <jessica.aquino.torrez@gmail.com>
2020-07-10 20:29:01 -0400

    Arreglos buenos dias

--- fechaHora.py
+++ fechaHora.py
@@ -1,13 +1,14 @@
 #Todo lo referente a fechas y horas
 from datetime import datetime
 
-rightNow = datetime.now()
-
 def fechaActual(): #Retorna la fecha en formato YYYY-mm-dd
+    rightNow = datetime.now()
     return rightNow.strftime("%Y-%m-%d")
 
 def fechaHoraActual(): #Retorna la fecha en formato YYYY-mm-dd
+    rightNow = datetime.now()
     return rightNow.strftime("%Y-%m-%d (%H:%M:%S)")
 
 def horaActualInt(): #Retorna un entero de la hora actual (0-23)
+    rightNow = datetime.now()
     return int(rightNow.strftime("%H"))
